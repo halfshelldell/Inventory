56c94bfd0c7f4a46510878b2ec5330824c02da95
halfshelldell <simmonslydell@gmai.com>
2016-05-23 15:38:33 -0400

    added static methods create, remove, and update items

--- src/com/theironyard/Item.java
+++ src/com/theironyard/Item.java
@@ -3,13 +3,16 @@ package com.theironyard;
 /**
  * Created by illladell on 5/23/16.
  */
-public class Inventory {
+public class Item {
     String text;
     int quantity;
 
-    public Inventory(String text, int quantity) {
+    public Item(String text, int quantity) {
         this.text = text;
         this.quantity = quantity;
     }
+
+
+
 }
 
--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,36 +5,63 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<Inventory> items = new ArrayList<>();
+    public static void createItem(Scanner scanner, ArrayList<Item> items) {
+        System.out.println("Enter your item");
+        String text = scanner.nextLine();
+        Item item = new Item(text, 1);
+        items.add(item);
+    }
+
+    public static void removeItem(Scanner scanner, ArrayList<Item> items) {
+        System.out.println("Enter number of item(s) you would to remove:");
+        String rmvItems = scanner.nextLine();
+        int remove = Integer.valueOf(rmvItems);
+        items.remove(remove - 1);
+    }
+
+    public static void updateItem(Scanner scanner, ArrayList<Item> items) {
+        System.out.println("Enter which item you would like to update");
+        String updateNum = scanner.nextLine();
+        System.out.println("Update item's quantity");
+        String itemNum = scanner.nextLine();
+        int num = Integer.valueOf(updateNum);
+        Item tempItem = items.get(num - 1);
+        tempItem.quantity += Integer.valueOf(itemNum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Item:");
+
+
         while (true) {
+            for (Item item : items) {
+                System.out.println((1 + items.indexOf(item)) + ". " + "[" + item.quantity + "]" + " " + item.text);
+
+            }
             System.out.println("1: Create new item");
             System.out.println("2: Remove an item");
             System.out.println("3: Update item's quantity");
 
             String option = scanner.nextLine();
 
-
-
             switch (option) {
                 case "1":
-                    System.out.println("Enter your item");
-                    String text = scanner.nextLine();
-                    Inventory item = new Inventory(text, 1);
-                    items.add(item);
+                    createItem(scanner, items);
                     break;
 
                 case "2":
-                    // remove item
+                    removeItem(scanner, items);
                     break;
 
                 case "3":
-                    // update item quantity
+                    updateItem(scanner, items);
                     break;
 
-
+                default:
+                    System.out.println("Invalid option");
             }
         }
     }
